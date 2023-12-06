7907dbf2bdad626919040ba3b2b05a75ed3aede2
Jovan BIENVENU <B519MK@login.axa>
2023-12-06 09:09:57 +0100

    End day 01

--- 01/index.js
+++ 01/index.js
@@ -1,3 +1,7 @@
+import inputJson from "./input.js";
+
+// Part 1
+
 const extractFirstAndLastDigit = (word) => {
     const numbers = word.match(/\d/g);
     if (numbers) {
@@ -15,3 +19,57 @@ export const calculCalibration = (data) => {
   
   return result;
 };
+
+// Part 2
+
+const digitNumbers = {
+    "zero": "0",
+    "one": "1",
+    "two": "2",
+    "three": "3",
+    "four": "4",
+    "five": "5",
+    "six": "6",
+    "seven": "7",
+    "eight": "8",
+    "nine": "9"
+};
+
+const transformWordWithLettersToDigitWord = (word) => {
+  let result = "";
+
+  for(let i = 0; i < word.length; i++) {
+    const wordPart = word.substring(i, word.length);
+    // find first wordPart that match a digitNumber
+    const digitNumber = Object.keys(digitNumbers).find((digitNumber) => {
+      return wordPart.startsWith(digitNumber);
+    });
+
+    if (digitNumber) {
+      result += digitNumbers[digitNumber];
+      continue;
+    };
+
+    if (result.length === word.length) {
+      break;
+    }
+
+    result += word[i];
+  }
+
+  return result;
+};
+
+export const calculCalibrationWithLetters = (data) => {
+  let result = 0;
+  for(let i = 0; i < data.length; i++) {
+    const word = data[i];
+    const digitWord = transformWordWithLettersToDigitWord(word);
+    result += extractFirstAndLastDigit(digitWord);
+  }
+
+  return result;
+};
+
+console.log(calculCalibration(inputJson));
+console.log(calculCalibrationWithLetters(inputJson));
--- 01/index.test.js
+++ 01/index.test.js
@@ -1,10 +1,34 @@
-import { calculCalibration } from "./index";
+import { calculCalibration, calculCalibrationWithLetters } from "./index";
 
 describe('calculCalibration', () => {
-  test("Should calculCalibration with 142 when inputs are 1abc2, pqr3stu8vwx, a1b2c3d4e5f, treb7uchet returns 142", () => {
+  test("Should calculCalibration when inputs are 1abc2, pqr3stu8vwx, a1b2c3d4e5f, treb7uchet returns 142", () => {
     const inputs = ["1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet"];
     const expected = 142;
     const result = calculCalibration(inputs);
     expect(result).toEqual(expected);
-  })
+  });
+
+  test("Should calculCalibration when inputs are 1abc3, pqr3stuvwx, treb17uchet returns 63", () => {
+    const inputs = ["1abc3", "pqr3stuvwx", "treb17uchet"];
+    const expected = 63;
+    const result = calculCalibration(inputs);
+    expect(result).toEqual(expected);
+  });
+
+  test("Should calculCalibration when inputs are abc, stuvwx, trebuchet returns 0", () => {
+    const inputs = ["abc", "stuvwx", "trebuchet"];
+    const expected = 0;
+    const result = calculCalibration(inputs);
+    expect(result).toEqual(expected);
+  });
 });
+
+describe("calculCalibrationWithLetters", () => {
+  test("Should calculCalibrationWithLetters when inputs are two1nine, eightwothree, abcone2threexyz, xtwone3four, 4nineeightseven2, zoneight234, 7pqrstsixteen returns 281", () => {
+    const inputs = ["two1nine", "eightwothree", "abcone2threexyz", "xtwone3four", "4nineeightseven2", "zoneight234", "7pqrstsixteen"];
+    const expected = 281;
+    const result = calculCalibrationWithLetters(inputs);
+    expect(result).toEqual(expected);
+  });
+});
+
